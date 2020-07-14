bdc25afc9efd588e27f82426d28097dbe60738a8
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-07-14 07:00:32 -0400

    Database folder is sucessfully running.

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const usersRouter = require("./users/users-routers");
+
+const server = express();
+const port = process.env.Port || 2500;
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(cookieParser());
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Something went wrong" });
+});
+
+server.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
+});
