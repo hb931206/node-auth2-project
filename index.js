64b01601d2199d908e0ba7965d132b4433146e70
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-07-14 08:03:59 -0400

    Hit MVP

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const usersRouter = require("./users/users-routers");
+const usersRouter = require("./users/users-router");
 
 const server = express();
 const port = process.env.Port || 2500;
@@ -11,6 +11,8 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(cookieParser());
+
+server.use(usersRouter);
 
 server.use((err, req, res, next) => {
   console.log(err);
